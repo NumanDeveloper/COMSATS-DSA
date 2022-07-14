14edc3dead6f08125f8b5bdbd7e2d7d6366e6635
numan-iftikhar <numaniftikhar300@gmail.com>
2022-07-14 14:16:46 +0500

    minor updation

--- Arrays/10-array-insertion.c
+++ Arrays/10-array-insertion.c
@@ -5,39 +5,41 @@
 void display(int arr[], int n)
 {
     // traversal
+    printf("Traversing...\n");
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%d | ", arr[i]);
     }
     printf("\n");
 }
 
-int indInsertion(int arr[], int size, int element, int capacity, int index)
+int indexInsertion(int arr[], int size, int element, int index, int capacity)
 {
     // insertion
     if (size >= capacity)
     {
         return -1; // no insertion
     }
+
     for (int i = size - 1; i >= index; i--)
     {
-        arr[i + 1] = arr[i]; // moving element one index furthur and making space for new element
+        arr[i + 1] = arr[i]; // moving element one index forward for making space for new element
     }
     arr[index] = element;
     return 1; //tells successful completion
 }
+
 int main()
 {
     system("cls");
 
     int arr[100] = {7, 8, 12, 27, 88};
-    int size = 5, element = 45, //element for insertion
-        index = 3;              //index at which insertion is required
+    int size = 5;
 
     printf("Array before insertion\n");
     display(arr, size);
 
-    indInsertion(arr, size, element, 100, index); //calling insertion function
+    indexInsertion(arr, size, 25, 3, 100); //calling insertion function
 
     printf("Array after insertion\n");
     size += 1;          //incrementing size
